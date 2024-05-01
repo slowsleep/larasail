5ec509879368eeeb1c06195022f5edac3a4c5e7c
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-05-01 14:22:48 +0300

    Add migration for games table

---
+++ database/migrations/2024_04_30_095238_create_games_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('genre')->nullable();
+            $table->string('developer')->nullable();
+            $table->string('publisher')->nullable();
+            $table->string('comment')->nullable();
+            $table->boolean('finished');
+            $table->boolean('abandoned');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
