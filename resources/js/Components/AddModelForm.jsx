2e35603f1b33f966892a6166bd7dd095dd5bc463
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-05-08 19:01:16 +0300

    Add custom class for input element in AddModelForm

--- resources/js/Components/AddModelForm.jsx
+++ resources/js/Components/AddModelForm.jsx
@@ -5,7 +5,10 @@ export default function AddModelForm({id, listInputs, ...props}) {
             {listInputs.map((item) => (
                 <div className={"flex flex-1 flex-col " + (item.type == "checkbox" || item.type == "number" ? "items-center" : "")} key={item.name}>
                     <label>{item.label}</label>
-                    <input {...item} className={(item.type == "checkbox" ? "m-1" : "text-black") + " " + (item.type == "number" ? "w-20" : "")} />
+                    <input
+                        className={(item.type == "checkbox" ? "m-1" : "text-black") + " " + (item.type == "number" ? "w-20" : "") + (item.class ? " " + item.class : "")}
+                        {...item}
+                    />
                  </div>
             ))}
             <button className="p-4 rounded bg-emerald-600 hover:bg-emerald-800" type="submit">Добавить</button>
