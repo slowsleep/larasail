f0e87b0e22b6962e1797445e05a3e863e1c60f9d
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-05-12 20:08:53 +0300

    Add links to tables on dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 
 export default function Dashboard({ auth }) {
     return (
@@ -14,11 +14,31 @@ export default function Dashboard({ auth }) {
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
                         <div className="px-6 py-6 flex gap-x-8">
-                            <div className="flex-1 p-2 border-2 border-cyan-400 bg-cyan-400/20 hover:bg-cyan-400/40 text-white text-center cursor-pointer">фильмы</div>
-                            <div className="flex-1 p-2 border-2 border-pink-400 bg-pink-400/20 hover:bg-pink-400/40 text-white text-center cursor-pointer">сериалы</div>
-                            <div className="flex-1 p-2 border-2 border-violet-600 bg-violet-600/20 hover:bg-violet-600/40 text-white text-center cursor-pointer">игры</div>
-                            <div className="flex-1 p-2 border-2 border-amber-800 bg-amber-800/20 hover:bg-amber-800/40 text-white text-center cursor-pointer">книги</div>
-                            <div className="flex-1 p-2 border-2 border-emerald-600 bg-emerald-600/20 hover:bg-emerald-600/40 text-white text-center cursor-pointer">манга</div>
+                            <Link href={route('movies')} className="flex-1 p-2 border-2 border-cyan-400 bg-cyan-400/20 hover:bg-cyan-400/40 text-white text-center cursor-pointer">
+                                <div>
+                                    фильмы
+                                </div>
+                            </Link>
+                            <Link className="flex-1 p-2 border-2 border-pink-400 bg-pink-400/20 hover:bg-pink-400/40 text-white text-center cursor-pointer" href={route('series')}>
+                                <div>
+                                    сериалы
+                                </div>
+                            </Link>
+                            <Link className="flex-1 p-2 border-2 border-violet-600 bg-violet-600/20 hover:bg-violet-600/40 text-white text-center cursor-pointer" href={route('games')}>
+                                <div>
+                                    игры
+                                </div>
+                            </Link>
+                            <Link className="flex-1 p-2 border-2 border-amber-800 bg-amber-800/20 hover:bg-amber-800/40 text-white text-center cursor-pointer" href={route('books')}>
+                                <div>
+                                    книги
+                                </div>
+                            </Link>
+                            <Link className="flex-1 p-2 border-2 border-emerald-600 bg-emerald-600/20 hover:bg-emerald-600/40 text-white text-center cursor-pointer">
+                                <div>
+                                    манга
+                                </div>
+                            </Link>
                         </div>
                     </div>
                 </div>
