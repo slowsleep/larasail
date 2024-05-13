1cec5cf6ae3449c63f342c5acdc609f29b953663
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-05-13 12:49:45 +0300

    Add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
+
         <!-- Scripts -->
         @routes
         @viteReactRefresh
