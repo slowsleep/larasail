25460276a8bba23c11922703a6d3c9e8793e1197
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-06-14 21:22:04 +0300

    Add NewMessageNotification

---
+++ app/Notifications/NewMessageNotification.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+
+class NewMessageNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public $message;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via($notifiable)
+    {
+        return ['broadcast'];
+    }
+
+    /**
+     * Get the broadcast representation of the notification.
+     */
+    public function toBroadcast($notifiable)
+    {
+        return new BroadcastMessage([
+            'message' => $this->message->load('user'),
+        ]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
