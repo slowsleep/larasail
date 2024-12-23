395020772f2ff80eab78d86ba26d3b8fbbc19b66
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-12-23 20:14:02 +0300

    Edit ModelRow component: add hiding the cells in the table row

--- resources/js/Components/ModelRow.jsx
+++ resources/js/Components/ModelRow.jsx
@@ -1,11 +1,18 @@
 import { useEffect, useState } from 'react'
 
 /**
- * @param {string} modelName склонить в винительном падеже
+ * @param {object} modelName ru - в единственном числе, en - как в БД
  */
 export default function ModelRow({className, inputs, data, setData, modelItem, modelName, onSave, onDestroy, onCancle, ...props}) {
     const [isEdit, setIsEdit] = useState(false);
     const [isFormError, setIsFormError] = useState(false);
+    const [tableCols, setTableCols] = useState(JSON.parse(localStorage.getItem('tableColumnsVisibility')) ?? {});
+    const [modelTableCols, setModelTableCols] = useState(tableCols[modelName.en] ?? []);
+
+    useEffect(() => {
+        let newModelTableCols = JSON.parse(localStorage.getItem('tableColumnsVisibility'))[modelName.en] ?? [];
+        setModelTableCols(newModelTableCols);
+    }, [isEdit]);
 
     useEffect(() => {
         if (data.abandoned) {
@@ -19,7 +26,7 @@ export default function ModelRow({className, inputs, data, setData, modelItem, m
     }, [inputs])
 
     const handleDestroy = () => {
-        if (confirm(`Вы уверены, что хотите удалить ${modelName} "${modelItem.title}"?`)) {
+        if (confirm(`Вы уверены, что хотите удалить ${modelName.ru} "${modelItem.title}"?`)) {
             onDestroy();
         }
     }
@@ -42,8 +49,15 @@ export default function ModelRow({className, inputs, data, setData, modelItem, m
     return (
         <tr className={className + " " + (data.abandoned ? "brightness-50 grayscale" : "")} {...props}>
             {inputs.map((item) => {
+                let isHidden = false;
+
+                if (modelTableCols && modelTableCols.length > 0) {
+                    let columnData = modelTableCols.filter(col => col.label == item.name) ?? [];
+                    isHidden = columnData[0]?.hidden;
+                }
+
                 if (item.type == "checkbox") {
-                    return <td className="p-2 text-white text-center" key={item.name}>
+                    return <td className={"p-2 text-white text-center" + (item.className ? " " + item.className : "") + (isHidden ? " hidden" : "")} key={item.name}>
                         {!isEdit ?
                             <input className="bg-transparent" type="checkbox" checked={item.value} disabled />
                             :
@@ -51,13 +65,14 @@ export default function ModelRow({className, inputs, data, setData, modelItem, m
                         }
                     </td>
                 } else {
-                    return <td className={"text-white" + (!isEdit ? " p-2" : "")} key={item.name}>
+                    const { error, ...filteredItem } = item;
+                    return <td className={"text-white" + (!isEdit ? " p-2" : "") + (item.className ? " " + item.className : "") + (isHidden ? " hidden" : "")} key={item.name}>
                         {!isEdit ?
                             <p className="w-full break-words">{item.value}</p>
                             :
                             <input
-                                className={"bg-transparent w-full" + (item.error && isFormError ? " border-red-500" : "")}
-                                {...item}
+                                {...filteredItem}
+                                className={"bg-transparent w-full" + (error && isFormError ? " border-red-500" : "")}
                             />
                         }
                     </td>
