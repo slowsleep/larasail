35dd98eadbc8afacc3050a72b917f3b3ace27e52
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-12-27 07:30:02 +0300

    fix: rename AuthenticatedLayout header prop in Anime page

--- resources/js/Pages/Anime/Anime.jsx
+++ resources/js/Pages/Anime/Anime.jsx
@@ -42,7 +42,7 @@ export default function Anime({auth, animeList, anime, action}) {
     return (
         <AuthenticatedLayout
         user={auth.user}
-        header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Книги</h2>}
+        header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Аниме</h2>}
     >
         <Head title="Anime" />
         <div className="py-12">
