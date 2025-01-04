8254a457dcc6acdbe17f2c7d716f9cca263251ba
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-01-04 07:39:36 +0300

    feat: add 'sort' api routes for anime, movies, series, mangas, games, books

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => ['auth', 'web', 'auth:sanctum']],function () {
     Route::patch('/anime', [App\Http\Controllers\Api\AnimeController::class, 'update'])->name('api.anime.update');
+    Route::get('/anime/sort', [App\Http\Controllers\Api\AnimeController::class, 'sort'])->name('api.anime.sort');
+
     Route::patch('/movies', [App\Http\Controllers\Api\MovieController::class, 'update'])->name('api.movies.update');
+    Route::get('/movies/sort', [App\Http\Controllers\Api\MovieController::class, 'sort'])->name('api.movies.sort');
+
     Route::patch('/series', [App\Http\Controllers\Api\SeriesController::class, 'update'])->name('api.series.update');
+    Route::get('/series/sort', [App\Http\Controllers\Api\SeriesController::class, 'sort'])->name('api.series.sort');
+
     Route::patch('/mangas', [App\Http\Controllers\Api\MangaController::class, 'update'])->name('api.mangas.update');
+    Route::get('/mangas/sort', [App\Http\Controllers\Api\MangaController::class, 'sort'])->name('api.mangas.sort');
+
     Route::patch('/games', [App\Http\Controllers\Api\GameController::class, 'update'])->name('api.games.update');
+    Route::get('/games/sort', [App\Http\Controllers\Api\GameController::class, 'sort'])->name('api.games.sort');
+
     Route::patch('/books', [App\Http\Controllers\Api\BookController::class, 'update'])->name('api.books.update');
+    Route::get('/books/sort', [App\Http\Controllers\Api\BookController::class, 'sort'])->name('api.books.sort');
 });
