427383bfc09e686bef3baee496b54abfce7c0c37
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-01-06 20:04:35 +0300

    fix: destructure item into className and filteredItem

--- resources/js/Components/AddModelForm.jsx
+++ resources/js/Components/AddModelForm.jsx
@@ -2,15 +2,18 @@ export default function AddModelForm({id, listInputs, ...props}) {
 
     return (
         <form className="w-full flex flex-col lg:flex-row dark:text-white items-center justify-between" id={id} {...props}>
-            {listInputs.map((item) => (
+            {listInputs.map((item) => {
+                let { className, ...filteredItem } = item;
+                return (
                 <div className={"flex flex-1 lg:flex-col flex-row items-center justify-between float-left w-full " + (item.type == "checkbox" || item.type == "number" ? "items-center" : "")} key={item.name}>
                     <label>{item.label}</label>
                     <input
-                        className={(item.type == "checkbox" ? "m-1" : "text-black") + " " + (item.type == "number" ? "w-20" : item.type == "text" ? "w-full" : "") + (item.className ? " " + item.className : "")}
-                        {...item}
+                        className={(item.type == "checkbox" ? "m-1" : "text-black") + " " + (item.type == "number" ? "w-20" : item.type == "text" ? "w-full" : "") + (className ? " " + className : "")}
+                        {...filteredItem}
                     />
                  </div>
-            ))}
+                )
+            })}
             <button className="p-4 rounded bg-emerald-600 hover:bg-emerald-800 w-full lg:w-auto" type="submit">Добавить</button>
         </form>
     )
