dc9e0e558d254054e0a048521a77be449c8aa74d
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-01-09 06:34:53 +0300

    feat: move table control buttons from ModelTableSorting and ShowingTableCol components to a new TableControls component

--- resources/js/Components/ModelTableSorting.jsx
+++ resources/js/Components/ModelTableSorting.jsx
@@ -6,10 +6,6 @@ import axios from "axios";
  * @returns
  */
 export default function ModelTableSorting({ model, columns, updateTableItems }) {
-    const showSortingMenu = () => {
-        document.querySelector("#sorting-menu").classList.toggle("hidden");
-    }
-
     const handleAsc = (col) => () => {
         axios.get(route('api.' + model + '.sort'), {
             params: {
@@ -66,45 +62,37 @@ export default function ModelTableSorting({ model, columns, updateTableItems }) 
     }
 
     return (
-        <>
-            <button className="border rounded border-gray-600 w-fit p-2" onClick={showSortingMenu}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
-                </svg>
-            </button>
-
-            <div className="p-2 border border-gray-600 my-2 overflow-y-auto hidden" id="sorting-menu">
-                <h3>Сортировка по:</h3>
-                <div className="flex flex-row justify-between">
-                    {columns.map((col, index) => (
-                        <div className="p-2 border border-gray-600 my-2 w-full" key={index}>
-                            <h4 className="text-center">{col}</h4>
-                            <div className="flex flex-row justify-center gap-x-2">
-                            {col == "status" ?
-                                <>
-                                    <button className="hover:bg-gray-300 p-1 rounded" onClick={handleInProgress(col)}>В процессе</button>
-                                    <button className="hover:bg-gray-300 p-1 rounded" onClick={handleFinished(col)}>Завершен</button>
-                                    <button className="hover:bg-gray-300 p-1 rounded" onClick={handleAbandoned(col)}>Заброшен</button>
-                                </>
-                                    :
-                                <>
-                                    <button className="hover:bg-gray-300 p-1 rounded" onClick={handleAsc(col)}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18" />
-                                        </svg>
-                                    </button>
-                                    <button className="hover:bg-gray-300 p-1 rounded" onClick={handleDesc(col)}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25 12 21m0 0-3.75-3.75M12 21V3" />
-                                        </svg>
-                                    </button>
-                                </>
-                            }
-                            </div>
+        <div className="p-2 border border-gray-600 my-2 overflow-y-auto hidden dark:bg-gray-700/50" id="sorting-menu">
+            <h3>Сортировка по:</h3>
+            <div className="flex flex-row justify-between">
+                {columns.map((col, index) => (
+                    <div className="p-2 border border-gray-600 my-2 w-full" key={index}>
+                        <h4 className="text-center">{col}</h4>
+                        <div className="flex flex-row justify-center gap-x-2">
+                        {col == "status" ?
+                            <>
+                                <button className="hover:bg-gray-300 p-1 rounded" onClick={handleInProgress(col)}>В процессе</button>
+                                <button className="hover:bg-gray-300 p-1 rounded" onClick={handleFinished(col)}>Завершен</button>
+                                <button className="hover:bg-gray-300 p-1 rounded" onClick={handleAbandoned(col)}>Заброшен</button>
+                            </>
+                                :
+                            <>
+                                <button className="hover:bg-gray-300 p-1 rounded" onClick={handleAsc(col)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18" />
+                                    </svg>
+                                </button>
+                                <button className="hover:bg-gray-300 p-1 rounded" onClick={handleDesc(col)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25 12 21m0 0-3.75-3.75M12 21V3" />
+                                    </svg>
+                                </button>
+                            </>
+                        }
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
-        </>
+        </div>
     );
 }
---
+++ resources/js/Components/Table/TableControls.jsx
@@ -0,0 +1,36 @@
+export default function TableControls({ children }) {
+    const showColsControl = () => {
+        document.querySelector("#cols-control").classList.toggle("hidden");
+    }
+
+    const showSortingMenu = () => {
+        document.querySelector("#sorting-menu").classList.toggle("hidden");
+    }
+    
+    return (
+        <>
+            <div className='flex flex-row gap-x-2'>
+                <button
+                    className="border rounded border-gray-600 w-fit p-2"
+                    onClick={showColsControl}
+                    title="Видимость колонок"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                </button>
+                <button
+                    className="border rounded border-gray-600 w-fit p-2"
+                    onClick={showSortingMenu}
+                    title="Сортировка"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+                    </svg>
+                </button>
+            </div>
+            {children}
+        </>
+    );
+}
