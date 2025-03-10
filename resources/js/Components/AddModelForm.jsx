12d0bf7ee5e81c72b0b060db37edafa59b9e1669
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-03-10 16:35:09 +0300

    feat: add support for select input in AddModelForm

--- resources/js/Components/AddModelForm.jsx
+++ resources/js/Components/AddModelForm.jsx
@@ -7,11 +7,28 @@ export default function AddModelForm({className = '', id, listInputs, ...props})
                 return (
                 <div className={"flex flex-1 lg:flex-col flex-row items-center justify-between float-left w-full " + (item.type == "checkbox" || item.type == "number" ? "items-center" : "")} key={item.name}>
                     <label>{item.label}</label>
-                    <input
-                        className={(item.type == "checkbox" ? "m-1" : "text-black") + " " + (item.type == "number" ? "w-20" : item.type == "text" ? "w-full" : "") + (className ? " " + className : "")}
-                        {...filteredItem}
-                    />
-                 </div>
+                    {item.type == "select" ?
+                        <select
+                            {...filteredItem}
+                            className={
+                                "bg-transparent " +
+                                (item.color ? " " + item.color : "") +
+                                (className ? " " + className : "")
+                            }
+                        >
+                            {Object.keys(item.options).map((key) => (
+                                <option key={key} value={key}>
+                                    {item.options[key]}
+                                </option>
+                            ))}
+                        </select>
+                    :
+                        <input
+                            className={(item.type == "checkbox" ? "m-1" : "text-black") + " " + (item.type == "number" ? "w-20" : item.type == "text" ? "w-full" : "") + (className ? " " + className : "")}
+                            {...filteredItem}
+                        />
+                    }
+                    </div>
                 )
             })}
             <button className="p-4 rounded bg-emerald-600 hover:bg-emerald-800 w-full lg:w-auto" type="submit">Добавить</button>
