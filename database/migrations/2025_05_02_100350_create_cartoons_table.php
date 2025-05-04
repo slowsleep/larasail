aeb84e285329ba5a010f79e28edeedabf57d3318
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-05-04 13:00:18 +0300

    feat: add create cartoons table migration

---
+++ database/migrations/2025_05_02_100350_create_cartoons_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cartoons', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('genre')->nullable();
+            $table->integer('year')->nullable();
+            $table->string('publisher')->nullable();
+            $table->string('comment')->nullable();
+            $table->foreignId('status_id')->constrained()->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cartoons');
+    }
+};
