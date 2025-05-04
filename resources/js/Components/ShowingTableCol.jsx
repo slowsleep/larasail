37dc1e31af64ec6e02fdd96ea54ec30c5f3131ef
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-05-04 17:01:51 +0300

    docs: add JSDoc comment for model param and edit comment for columns param in ShowingTableCol component

--- resources/js/Components/ShowingTableCol.jsx
+++ resources/js/Components/ShowingTableCol.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from 'react'
 import { hideTableColumn } from '@/Utils/tableUtils';
 
 /**
- * @param {array} columns - array of objects with column name (label) and column number, for example [{label: "title", column: 0}]
+ * @param {string} model - model name for localStorage (recommended camelCase)
+ * @param {array} columns - array of objects with column name like in DB and column number, for example [{label: "title", column: 0}]
  */
 export default function ShowingTableCol({model, columns, tableRef}) {
     const [tableCols, setTableCols] = useState(JSON.parse(localStorage.getItem('tableColumnsVisibility')) ?? {});
