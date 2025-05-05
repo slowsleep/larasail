0108b11f234156041c6b059d15e26aa6bbd17f51
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-05-05 12:44:33 +0300

    style: update layout and styling for ApplicationLogo in GuestLayout

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -4,9 +4,9 @@ import { Link } from '@inertiajs/react';
 export default function Guest({ children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-zinc-100 dark:bg-zinc-900">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-zinc-500" />
+            <div className="text-center">
+                <Link href="/" >
+                    <ApplicationLogo />
                 </Link>
             </div>
 
