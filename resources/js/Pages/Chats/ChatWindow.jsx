c6d173833d6e707c3a963c14652d71c3983ed2cd
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-05-17 16:49:13 +0300

    style: edit width, color bg messages, add style for chat title, remove 1 div

--- resources/js/Pages/Chats/ChatWindow.jsx
+++ resources/js/Pages/Chats/ChatWindow.jsx
@@ -68,38 +68,38 @@ export default function ChatWindow({ auth, chat }) {
     };
 
     return (
-        <div className="container mx-auto p-4 h-full">
-            <div className="flex flex-col p-4 bg-zinc-100 rounded w-full h-full overflow-y-auto">
+        <div className="h-full flex flex-col p-4 bg-zinc-100 rounded w-full overflow-y-auto">
+            <div className="bg-zinc-500 p-1 rounded mb-4">
                 <h4 className="text-lg font-bold text-center underline">chat name: {chat.name}</h4>
-                <div className="messages mb-4 flex flex-col overflow-y-auto">
-                    {messages.map(msg => (
-                        <div key={msg.id} className={"mb-2 p-1 bg-zinc-200 p-1 rounded px-2 " + (msg.user.id === auth.user.id ? 'ml-auto' : 'mr-auto')}>
-                            <p className={msg.user.id === auth.user.id ? 'text-right' : ''}><strong>{msg.user.name}</strong></p>
-                            <p>{msg.content}</p>
-                            <p className="text-sm text-end text-zinc-500" title={formatDate(msg.created_at)}>{formatTime(msg.created_at)}</p>
-                        </div>
-                    ))}
-                </div>
-                <form onSubmit={handleSendMessage} className="flex flex-row gap-x-2">
-                    <div className="w-full">
-                        <textarea
-                            value={message}
-                            onChange={(e) => setMessage(e.target.value)}
-                            name="message"
-                            className="w-full p-2 border rounded"
-                            rows="1"
-                            placeholder="Введите сообщение"
-                            onKeyDown={keyDownFunction}
-                            spellCheck="true"
-                            maxLength={4096}
-                            required
-                        />
-                    </div>
-                    <button type="submit" className="bg-blue-500 text-white px-4 rounded">
-                        Отправить
-                    </button>
-                </form>
             </div>
+            <div className="messages mb-4 flex-1 flex-col overflow-y-auto">
+                {messages.map(msg => (
+                    <div key={msg.id} className={"mb-2 w-full sm:w-1/2 p-1 p-1 rounded px-2 " + (msg.user.id === auth.user.id ? 'ml-auto bg-zinc-300' : 'mr-auto bg-zinc-200')}>
+                        <p className={msg.user.id === auth.user.id ? 'text-right' : ''}><strong>{msg.user.name}</strong></p>
+                        <p>{msg.content}</p>
+                        <p className="text-sm text-end text-zinc-500" title={formatDate(msg.created_at)}>{formatTime(msg.created_at)}</p>
+                    </div>
+                ))}
+            </div>
+            <form onSubmit={handleSendMessage} className="flex flex-row gap-x-2">
+                <div className="w-full">
+                    <textarea
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        name="message"
+                        className="w-full p-2 border rounded"
+                        rows="1"
+                        placeholder="Введите сообщение"
+                        onKeyDown={keyDownFunction}
+                        spellCheck="true"
+                        maxLength={4096}
+                        required
+                    />
+                </div>
+                <button type="submit" className="bg-blue-500 text-white px-4 rounded">
+                    Отправить
+                </button>
+            </form>
         </div>
     );
 };
